8ce3e085d502a3100d27e46bf958f9cd4f497c66
dai6658 <neolizard1149@gmail.com>
2025-06-03 00:09:02 +0700

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -1,3 +1,4 @@
+
 import re
 import string
 from underthesea import word_tokenize
@@ -5,7 +6,8 @@ from sentence_transformers import SentenceTransformer
 
 def load_stopwords(file_path="vietnamese-stopwords.txt"):
     with open(file_path, "r", encoding="utf-8") as f:
-        return set(line.strip() for line in f if line.strip())
+        return set(line.strip() for line in f if line.strip() and " " not in line.strip())
+
 stopwords_vn = load_stopwords()
 
 def load_model():
@@ -15,9 +17,8 @@ def remove_numbered_prefix(text):
     return re.sub(r"(?m)^\s*\d+\.\s*", "", text)
 
 def preprocess_sentence(sentence):
-    sentence = sentence.lower()
-    sentence = sentence.translate(str.maketrans("", "", string.punctuation))
+    sentence = sentence.lower().translate(str.maketrans("", "", string.punctuation))
     words = word_tokenize(sentence)
-    words = [w for w in words if w not in stopwords_vn]
-    return " ".join(words)
+    return " ".join([w for w in words if w not in stopwords_vn])
+
 
